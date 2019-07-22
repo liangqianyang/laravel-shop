df25d0d8f499219328862479875352d0b9c3c9c2
leo.liang <543492227@qq.com>
2019-07-22 15:07:48 +0800

    新增收货地址

--- resources/js/app.js
+++ resources/js/app.js
@@ -27,6 +27,10 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+// 引入省市组件
+require('./components/SelectDistrict');
+require('./components/UserAddressesCreateAndEdit');
+
 const app = new Vue({
     el: '#app',
 });
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,9 @@ Route::get('/', 'PagesController@root')->name('root');//主页
 Auth::routes();
 
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
-Route::group(['middleware' => ['auth']], function() {
+Route::group(['middleware' => ['auth']], function () {
     Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+    Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+    Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
 });
 
