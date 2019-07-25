2de159fe2221cd810f6da7c9b17318dd2ba9c3ba
leo.liang <543492227@qq.com>
2019-07-25 17:03:42 +0800

    购物车页面

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,8 @@ Route::group(['middleware' => ['auth']], function () {
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');//取消收藏
     Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');//收藏列表
     Route::post('cart', 'CartController@add')->name('cart.add');//添加购物车
+    Route::get('cart', 'CartController@index')->name('cart.index');//购物车列表
+    Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');//移除购物车
 });
 
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');//商品详情页
