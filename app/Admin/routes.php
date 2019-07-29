bbd43b07109b1826ff3047cf1d94cae8b2663c4b
leo.liang <543492227@qq.com>
2019-07-29 17:28:23 +0800

    管理后台-新增,修改,删除优惠券

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -22,4 +22,9 @@ Route::group([
     $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship');//商品发货
     $router->post('orders/{order}/refund', 'OrdersController@handleRefund')->name('admin.orders.handle_refund');//拒绝退款
     $router->get('coupon_codes', 'CouponCodesController@index');//优惠券列表
+    $router->post('coupon_codes', 'CouponCodesController@store');//添加优惠券
+    $router->get('coupon_codes/create', 'CouponCodesController@create');//添加优惠券的页面
+    $router->get('coupon_codes/{id}/edit', 'CouponCodesController@edit');//编辑优惠券的页面
+    $router->put('coupon_codes/{id}', 'CouponCodesController@update');//更新优惠券的页面
+    $router->delete('coupon_codes/{id}', 'CouponCodesController@destroy');//删除优惠券
 });
