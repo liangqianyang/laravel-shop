0fb8cdc7726ab0d0f7fdc440fb10ac5792e3109e
leo.liang <543492227@qq.com>
2019-07-29 16:40:27 +0800

    后台管理-同意退款-支付宝退款

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Ramsey\Uuid\Uuid;
 class Order extends Model
 {
     const REFUND_STATUS_PENDING = 'pending';
@@ -94,6 +94,11 @@ class Order extends Model
         return $this->hasMany(OrderItem::class);
     }
 
+    /**
+     * 生成流水号
+     * @return bool|string
+     * @throws \Exception
+     */
     public static function findAvailableNo()
     {
         // 订单流水号前缀
@@ -109,5 +114,20 @@ class Order extends Model
         \Log::warning('find order no failed');
 
         return false;
+    }
+
+    /**
+     * 生成退款单号
+     * @return string
+     */
+    public static function getAvailableRefundNo()
+    {
+        do {
+            // Uuid类可以用来生成大概率不重复的字符串
+            $no = Uuid::uuid4()->getHex();
+            // 为了避免重复我们在生成之后在数据库中查询看看是否已经存在相同的退款订单号
+        } while (self::query()->where('refund_no', $no)->exists());
+
+        return $no;
     }
 }
