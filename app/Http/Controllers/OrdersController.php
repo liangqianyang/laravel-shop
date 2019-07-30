75aef6d6373ce7d25b64df4096d821dfc013c550
leo.liang <543492227@qq.com>
2019-07-30 09:57:03 +0800

    用户界面-使用优惠券下单

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -12,6 +12,9 @@ use Carbon\Carbon;
 use App\Http\Requests\SendReviewRequest;
 use App\Events\OrderReviewed;
 use App\Http\Requests\ApplyRefundRequest;
+use App\Exceptions\CouponCodeUnavailableException;
+use App\Models\CouponCode;
+
 class OrdersController extends Controller
 {
 
@@ -41,10 +44,17 @@ class OrdersController extends Controller
      */
     public function store(OrderRequest $request, OrderService $orderService)
     {
-        $user    = $request->user();
+        $user = $request->user();
         $address = UserAddress::find($request->input('address_id'));
-
-        return $orderService->store($user, $address, $request->input('remark'), $request->input('items'));
+        $coupon = null;
+        // 如果用户提交了优惠码
+        if ($code = $request->input('coupon_code')) {
+            $coupon = CouponCode::where('code', $code)->first();
+            if (!$coupon) {
+                throw new CouponCodeUnavailableException('优惠券不存在');
+            }
+        }
+        return $orderService->store($user, $address, $request->input('remark'), $request->input('items'), $coupon);
     }
 
 
@@ -136,8 +146,8 @@ class OrdersController extends Controller
                 $orderItem = $order->items()->find($review['id']);
                 // 保存评分和评价
                 $orderItem->update([
-                    'rating'      => $review['rating'],
-                    'review'      => $review['review'],
+                    'rating' => $review['rating'],
+                    'review' => $review['review'],
                     'reviewed_at' => Carbon::now(),
                 ]);
             }
@@ -171,12 +181,12 @@ class OrdersController extends Controller
             throw new InvalidRequestException('该订单已经申请过退款，请勿重复申请');
         }
         // 将用户输入的退款理由放到订单的 extra 字段中
-        $extra                  = $order->extra ?: [];
+        $extra = $order->extra ?: [];
         $extra['refund_reason'] = $request->input('reason');
         // 将订单退款状态改为已申请退款
         $order->update([
             'refund_status' => Order::REFUND_STATUS_APPLIED,
-            'extra'         => $extra,
+            'extra' => $extra,
         ]);
 
         return $order;
