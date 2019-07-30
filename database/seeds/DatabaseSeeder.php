41b638c99feae3442980d8de46aeda15efe93988
leo.liang <543492227@qq.com>
2019-07-30 14:45:01 +0800

    类目 Seeder文件

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,10 +6,11 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        $this->call(UsersSeeder::class);
         $this->call(UserAddressesSeeder::class);
-        $this->call(ProductsSeeder::class);
         $this->call(CouponCodesSeeder::class);
+        // 放在 ProductsSeeder 之前
+        $this->call(CategoriesSeeder::class);
+        $this->call(ProductsSeeder::class);
         $this->call(OrdersSeeder::class);
     }
 }
