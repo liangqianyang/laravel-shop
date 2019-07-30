f7d792a27696b71a8fd3a62cf6b3f937da99bf5a
leo.liang <543492227@qq.com>
2019-07-30 10:24:34 +0800

    完善假数据填充

--- app/Jobs/CloseOrder.php
+++ app/Jobs/CloseOrder.php
@@ -44,7 +44,7 @@ class CloseOrder implements ShouldQueue
             if ($this->order->couponCode) {
                 $this->order->couponCode->changeUsed(false);
             }
-            
+
             // 将订单的 closed 字段标记为 true，即关闭订单
             $this->order->update(['closed' => true]);
             // 循环遍历订单中的商品 SKU，将订单中的数量加回到 SKU 的库存中去
